407fc606b15b5852d963239ecfcc5d177f34e2c8
rjfilipp <rjfilipp>
2017-06-20 22:15:37 -0700

    Add Apr2017.js

---
+++ Apr2017.js
@@ -0,0 +1,36 @@
+﻿var z = '\u200B';
+
+
+var guanjian = "http://s3.amazonaws.com/chinesepod.com/0658/ab3bbfaee73de2c713691929a2ee5fb8bf6ce04a/mp3/64/rec-1190369849734-2.mp3";
+var yimian = "http://s3.amazonaws.com/chinesepod.com/extra/QW0387/ef70591c917a7d2293d9de4240eaf17798c8240d/mp3/64/chinesepod_7277_dialogue_67859_prototype_56213_4927.mp3";
+var stranger = "http://s3.amazonaws.com/chinesepod.com/0689/053888474c66861c96866b2958ca53d49150bd4a/mp3/64/rec-1193732966966-26.mp3";
+var shuxi = "http://s3.amazonaws.com/chinesepod.com/0963/17dfb4257317857abd18c902076822ea90a2ab36/mp3/64/rec-1218041095078-14.mp3";
+var jixu = "http://s3.amazonaws.com/chinesepod.com/1055/7a54adcc778c2621ba7ec777bdc481e8db26984e/mp3/64/rec-1229040246609-18.mp3";
+var jiayou = "http://s3.amazonaws.com/chinesepod.com/1311/580af4477439e631d21a0491b1d385a7f700b160/mp3/64/chinesepod_4211_dialogue_30675_prototype_52146_235476.mp3";
+var sui = "http://s3.amazonaws.com/chinesepod.com/1225/a000d6b507c2cfce96256a292fd9d3b1dc15d44a/mp3/64/chinesepod_3369_dialogue_20975_prototype_142135_128529_88362_56377.mp3";
+var jianyi = "http://s3.amazonaws.com/chinesepod.com/0941/2b9490ba272546cfe84c51cfbe3e5c0ee5750a68/mp3/64/rec-1215473230171-9.mp3";
+var jinkuai = "http://s3.amazonaws.com/chinesepod.com/0731/14405243ae75f53a628d01b0df430e1fc711df9c/mp3/64/rec-1197443477140-19.mp3";
+var chenggong = "http://s3.amazonaws.com/chinesepod.com/0104/7619aeda0c47369a294c2aa4111332f60a7c4af1/mp3/64/rec-1213822679859-4.mp3";
+var youyong = "http://s3.amazonaws.com/chinesepod.com/2930/8bb174de16e8d9058da7ba5a387665cf9e83c931/mp3/64/chinesepod_7549_dialogue_71276_prototype_15806_48001.mp3";
+var tongzhi = "http://s3.amazonaws.com/chinesepod.com/1814/226ca66237f64fb9e1e3d583e113fab73793c3c9/mp3/64/chinesepod_6240_dialogue_54490_prototype_52682_57072.mp3";
+var renwei = "http://s3.amazonaws.com/chinesepod.com/0723/2eb42040ff4cbdf38f7cf31acffdbd4b80eb9442/mp3/64/rec-1196927529578-25.mp3";
+var yanzhong = "http://s3.amazonaws.com/chinesepod.com/0684/12e3d139db61ea14707eb61c4fddd197c518a527/mp3/64/rec-1193645679046-23.mp3";
+
+
+var apr2017 = [
+  [guanjian, "我在這裡要告訴"+z+"大家的是，成功"+z+"的關鍵是不斷"+z+"自我提升！", "我在这里要告诉"+z+"大家的是，成功"+z+"的关键是不断"+z+"自我提升！", "Wǒ zài zhèlǐ yào gàosu dàjiā de shì, chénggōng de guānjiàn shì bùduàn zìwǒ tíshēng!" , "What I want to tell everyone here is that the key to success is continual self improvement!"],
+  [yimian, "給小狗吃東西，"+z+"以免他餓死。", "给小狗吃东西，"+z+"以免他饿死。", "Gěi xiǎo gǒu chī dōngxi, yǐmiǎn tā è sǐ.", "Give the dog something to eat so that he won't starve to death."],
+  [stranger, "我跟陌生人說話"+z+"會害羞。", "我跟陌生人说话"+z+"会害羞。", "Wǒ gēn mòshēng rén shuōhuà huì hàixiū.", "I get shy when talking with strangers."],
+  [shuxi, "我想中午請"+z+"大家吃飯，"+z+"順便熟悉一下。", "我想中午请"+z+"大家吃饭，"+z+"顺便熟悉一下。", "Wǒ xiǎng zhōngwǔ qǐng dàjiā chīfàn, shùnbiàn shúxī yīxià.", "I want to treat everyone to lunch, and while we're at it, get to know each other a bit."],
+  [jixu, "你還想繼續學中文"+z+"嗎？", "你还想继续学中文"+z+"吗？", "Nǐ hái xiǎng jìxù xué zhōngwén ma?", "Do you still want to keep studying Chinese?"],
+  [jiayou, "再堅持一下，"+z+"加油！", "再坚持一下，"+z+"加油！", "Zài jiānchí yīxià, jiāyóu!", "Hold on just a bit longer. You can do it!"],
+  [sui, "好吧。我去洗衣服"+z+"了。你小心點兒，"+z+"別把碗打碎。", "好吧。我去洗衣服"+z+"了。你小心点儿，"+z+"别把碗打碎。", "Hǎo ba. Wǒ qù xǐ yīfúle. Nǐ xiǎoxīn diǎn er, bié bǎ wǎn dǎ suì.", "Fine. I’m going to wash the clothes. Be careful you don’t break the bowls."],
+  [jianyi, "他建議我們明天"+z+"早點兒去。", "他建议我们明天"+z+"早点儿去。", "Tā jiànyì wǒmen míngtiān zǎodiǎn er qù.", "He recommends that we go a little earlier tomorrow."],
+  [jinkuai, "這裡不太安全，"+z+"我想盡快搬家。", "这里不太安全，"+z+"我想尽快搬家。", "Zhèlǐ bù tài ānquán, wǒ xiǎng jǐnkuài bānjiā.", "It is not very safe here, I want to move as soon as I can."],
+  [chenggong, "如果我努力工作，"+z+"我想我會成功。", "如果我努力工作，"+z+"我想我会成功。", "Rúguǒ wǒ nǔlì gōngzuò, wǒ xiǎng wǒ huì chénggōng.", "If I work hard, I think I will be successful."],
+  [youyong, "我很少運動，"+z+"有時候去游泳。", "我很少运动，"+z+"有时候去游泳。", "Wǒ hěn shǎo yùndòng, yǒu shíhòu qù yóuyǒng.", "I don't exercise very often, sometimes I go swimming."],
+  [tongzhi, "謝謝你來參加"+z+"面試。結果我們"+z+"過幾天會通知你。", "谢谢你来参加"+z+"面试。结果我们"+z+"过几天会通知你。", "Xièxiè nǐ lái cānjiā miànshì. Jiéguǒ wǒmen guòjǐtiān huì tōngzhī nǐ.", "Thanks for coming in for an interview. We'll notify you of the results in a few days."],
+  [renwei, "很多人認為"+z+"“4”是一個"+z+"不吉利的數字。", "很多人认为"+z+"“4”是一个"+z+"不吉利的数字。", "Hěnduō rén rènwéi “4” shì yīgè bù jílì de shùzì.", "A lot of people think that the number four is an unlucky number."],
+  [yanzhong, "昨天我犯了一個"+z+"很嚴重的錯誤。", "昨天我犯了一个"+z+"很严重的错误。", "Zuótiān wǒ fànle yīgè hěn yánzhòng de cuòwù.", "Yesterday I committed a serious mistake."]
+  
+];
