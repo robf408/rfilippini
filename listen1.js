0797b44cc09db82139864560a67a0e542ccf4a8d
rjfilipp <rjfilipp>
2017-11-16 08:24:47 -0800

    Add listen1.js

---
+++ listen1.js
@@ -0,0 +1,21 @@
+﻿var z = '\u200B';
+
+var listen1_pre = "Based on: <a href=\"https://www.youtube.com/watch?v=G1DA04gmVE4\">Listen and Learn 1</a><ul>" +
+"<li>Time: 00:00 <a href=\"https://hanyufanting.com/2017/06/16/new-practical-audio-visual-chinese-lesson-18-part-1/\">Unit 1</a></li>\n" +
+"<li>Time: 00:00 <a href=\"http://m.blog.naver.com/asian64/40193700079\">Unit 6</a></li>\n" +
+"</ul>";
+
+
+var cp = "http://s3.amazonaws.com/chinesepod.com/";
+var banjia = cp + "2999/07d1ba1d59af4e304724cebb620ad380278f04e2/mp3/64/chinesepod_7661_dialogue_72569_prototype_20115_9162_20242_23202_41151_15358.mp3";
+var gaitian = cp + "2779/fb28e82291581b541426d64d9755e9c6ef395e58/mp3/64/chinesepod_7390_dialogue_69289_prototype_37369_51848.mp3";
+var naqilai = cp + "1983/1486bd5a4f8220161589bc21b9251ac1ccc7201f/mp3/64/chinesepod_6484_dialogue_57800_prototype_5735_29417.mp3";
+
+var listen1 = [
+  [banjia, "沒辦法啊，自從"+z+"我搬家了以後，"+z+"幾乎餐餐都在外面"+z+"解決。", "没办法啊，自从"+z+"我搬家了以后，"+z+"几乎餐餐都在外面"+z+"解决。", "méi bànfǎ ā，zìcóng wǒ bānjiā le yǐhòu，jīhū cāncān dōu zài wàimian jiějué。", "I've got no other choice. Since I moved house, I eat almost all of my meals out."],
+  [gaitian, "慢走，改天聊。", "慢走，改天聊。", "màn zǒu，gǎitiān liáo。", "I'll let you go then, we can catch up another day."],
+  [naqilai, "餐布要放在腿上，"+z+"如果要擦嘴巴，"+z+"就這樣，"+z+"拿起來擦一下，"+z+"然後再放回去。", "餐布要放在腿上，"+z+"如果要擦嘴巴，"+z+"就这样，"+z+"拿起来擦一下，"+z+"然后再放回去。", "cānbù yào fàng zài tuǐ shàng，rúguǒ yào cā zuǐbā，jiù zhèyàng，ná qǐlái cā yīxià，ránhòu zài fàng huíqu。", "You should put your napkin on your lap. If you need to wipe your mouth, you just pick up your napkin like this, wipe your mouth, then put it back."]
+  
+  
+  
+];
