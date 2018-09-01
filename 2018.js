dbdb0840f285ad4a3673d53d8717fc4d2955149d
robf408 <robf408@yahoo.com>
2018-08-31 17:08:24 -0700

    Add 2018.js

---
+++ 2018.js
@@ -0,0 +1,9 @@
+﻿var z = '\u200B';
+
+var cp = "http://s3.amazonaws.com/chinesepod.com/";
+var nazou = cp + "1114/dfd55367b88cb67cab9893be275018c96359bedc/mp3/64/rec-1234315094906-13.mp3"
+
+
+var v2018 = [
+  [nazou, "那本書被她"+z+"拿走了。", "那本书被她"+z+"拿走了。", "nà běn shū bèi tā názǒu le。", "That book was taken away by her."]
+];
