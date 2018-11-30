5fa602293129ebd96fbcadefa896cf8e2734c234
robf408 <robf408@yahoo.com>
2018-11-29 21:00:17 -0800

    Mostly 2018 stuff

--- 2018.js
+++ 2018.js
@@ -2,8 +2,34 @@
 
 var cp = "http://s3.amazonaws.com/chinesepod.com/";
 var nazou = cp + "1114/dfd55367b88cb67cab9893be275018c96359bedc/mp3/64/rec-1234315094906-13.mp3"
-
-
+var laizi = cp + "1111/83fc0b837a3bb30f6865755c8eb5d6b23423b659/mp3/64/rec-1234208242484-28.mp3"
+var jiangjiu = cp + "1004/a6b1ccb05a7d308b5dcd51ec38aa4af4602488a9/mp3/64/rec-1222534251187-29.mp3";
+var yanjiu = cp + "0093/5f748e26401a0269ff9c2f48547c2a5d4798acfc/mp3/64/rec-1215102554734-14.mp3";
+var jiezhe = cp + "2039/3b731e3db667c1203b7e8440ca8a5449cf4786a9/mp3/64/chinesepod_6562_dialogue_58835_prototype_142601_184545_34159_118484.mp3";
+var tiyi = cp + "extra/QW0377/66d0db26e938e9b317376a04eca313da27d80f36/mp3/64/chinesepod_7177_dialogue_66735_prototype_17879_302736.mp3";
+var touzi = cp + "0613/959b86441dce020d8f582c23b0ce63b8b6dee905/mp3/64/rec-1187248778625-27.mp3";
+var peifu = cp + "0971/c2a958c7284e50606d1e67f944828feb05bfadd3/mp3/64/rec-1219106273640-25.mp3";
+var jinguan = cp + "1496/efe948b35b1fc164813b486d2b84a6c4b56e8a8b/mp3/64/chinesepod_5307_dialogue_45238_prototype_9667_20126.mp3";
+var dixia = cp + "2943/a18c73c824edcc9b4b25650986d34e7b1ec36377/mp3/64/chinesepod_7613_dialogue_72002_prototype_44834_33615.mp3";
+var pianjian = cp + "0843/0582cdca71735f66bfbb4a02b527785a07fc298c/mp3/64/rec-1206669402218-21.mp3";
+var yingpan = cp + "2787/31e257d6695568196e2a46a13cc169b473a3e501/mp3/64/chinesepod_7397_dialogue_69357_prototype_12016.mp3";
+var xingfen = cp + "0938/c9a2b8decc374b7860a6188b3550ec685b2310e9/mp3/64/rec-1215465156937-27.mp3";
+var weile = cp + "1426/30246e2e56b3618ee8622a80177dd3cc677572b7/mp3/64/chinesepod_4876_dialogue_39392_prototype_17520_94986.mp3";
+yinxiang = cp + "4010/c4252e59af4332ac24b10e50e961d1ce051cfc3d/mp3/64/chinesepod_7793_dialogue_74255_prototype_30445_44294_37881.mp3";
 var v2018 = [
-  [nazou, "那本書被她"+z+"拿走了。", "那本书被她"+z+"拿走了。", "nà běn shū bèi tā názǒu le。", "That book was taken away by her."]
+  [nazou, "那本書被她"+z+"拿走了。", "那本书被她"+z+"拿走了。", "nà běn shū bèi tā názǒu le。", "That book was taken away by her."],
+  [laizi, "美國是一個移民"+z+"國家，有來自"+z+"世界各地的人。", "美国是一个移民"+z+"国家，有来自"+z+"世界各地的人。", "Měiguó shì yī ge yímín guójiā，yǒu láizì shìjiè gèdì de rén。", "America is a nation of immigrants. It has people from all parts of the world."],
+  [jiangjiu, "你們隨便點就"+z+"好了，我對吃飯"+z+"沒什麼講究。","你们随便点就"+z+"好了，我对吃饭"+z+"没什么讲究。", "nǐmen suíbiàn diǎn jiù hǎo le，wǒ duì chīfàn méishénme jiǎngjiu。", "Just order whatever you like. I'm not that particular about eating."],
+  [yanjiu, "我經常使用互聯網"+z+"做研究。", "我经常使用互联网"+z+"做研究。", "wǒ jīngcháng shǐyòng hùliánwǎng zuò yánjiū。", "I often use the Internet to do research."],
+  [jiezhe, "中國朋友又接著"+z+"說：“沒關係啦，"+z+"那就以後吧。"+z+"以後找個你我都"+z+"方便的時候一起"+z+"吃飯。”", "中国朋友又接着"+z+"说：“没关系啦，"+z+"那就以后吧。"+z+"以后找个你我都"+z+"方便的时候一起"+z+"吃饭。”", "Zhōngguó péngyou yòu jiē zhe shuō：“méiguānxi la，nà jiù yǐhòu ba。yǐhòu zhǎo ge nǐ wǒ dōu fāngbiàn de shíhou yīqǐ chīfàn。”", "The Chinese friend continued again, \"It's OK. Some other time then. In the future we'll find a time that's convenient for both of us to have a meal together.\""],
+  [tiyi, "我表示我不接受"+z+"這種提議，"+z+"我覺得太不公平。", "我表示我不接受"+z+"这种提议，"+z+"我觉得太不公平。", "wǒ biǎoshì wǒ bù jiēshòu zhèzhǒng tíyì，wǒ juéde tài bù gōngpíng。", "I said that I don't accept this proposal, as I thought it was extremely unfair."],
+  [touzi, "現在股市這麼好，"+z+"我要調整一下"+z+"投資方向。", "现在股市这么好，"+z+"我要调整一下"+z+"投资方向。", "xiànzài gǔshì zhème hǎo，wǒ yào tiáozhěng yīxià tóuzī fāngxiàng。", "Right now the stock market is so good, I want to readjust my investment direction a bit."],
+  [peifu, "我真佩服你，"+z+"能做到工作家庭"+z+"兩不誤。", "我真佩服你，"+z+"能做到工作家庭"+z+"两不误。", "wǒ zhēn pèifú nǐ，néng zuò dào gōngzuò jiātíng liǎngbùwù。", "I really admire you being able to balance work and a family without neglecting either of them."],
+  [jinguan, "大家有什麼問題，"+z+"請儘管來找我。", "大家有什么问题，"+z+"请尽管来找我。", "dàjiā yǒu shénme wèntí，qǐng jǐnguǎn lái zhǎo wǒ。","If anyone has any questions, please feel free to come and talk to me."],
+  [dixia, "哎呀！"+z+"跑到桌子底下了！", "哎呀！"+z+"跑到桌子底下了！", "āiyā！pǎodào zhuōzi dǐxià le！", "Agh! It ran under the table!"],
+  [pianjian, "你對他有偏見，"+z+"無論他做什麼你都"+z+"覺得不對。", "你对他有偏见，"+z+"无论他做什么你都"+z+"觉得不对。", "nǐ duì tā yǒu piānjiàn，wúlùn tā zuò shénme nǐ dōu juéde bù duì。", "You are prejudiced against him. Regardless of what he does, you think it's wrong."],
+  [yingpan, "哎呀！電腦硬盤"+z+"空間又不夠了。", "哎呀！电脑硬盘"+z+"空间又不够了。", "āiyā！diànnǎo yìngpán kōngjiān yòu bù gòu le。", "Oh no! There's not enough space on the hard disk of my computer again."],
+  [xingfen, "看到屏幕上的數字"+z+"在瘋狂地跳動，他"+z+"也越來越興奮了。", "看到屏幕上的数字"+z+"在疯狂地跳动，他"+z+"也越来越兴奋了。", "kàndào píngmù shang de shùzì zài fēngkuáng de tiàodòng，tā yě yuèláiyuè xīngfèn le。", "Seeing the numbers on the screen jumping around like crazy, he got more and more excited."],
+  [weile, "為了大家的安全！", "为了大家的安全！", "wèile dàjiā de ānquán！", "It's for everyone's safety!"],
+  [yinxiang, "請你說說你在目前"+z+"的工作上，印象最"+z+"深刻的一件事。", "请你说说你在目前"+z+"的工作上，印象最"+z+"深刻的一件事。", "qǐng nǐ shuō shuo nǐ zài mùqián de gōngzuò shàng，yìnxiàng zuì shēnkè de yī jiàn shì。", "Please tell me about something that left the deepest impression on you in your current job."]
 ];
--- wotd.js
+++ wotd.js
@@ -8,6 +8,7 @@ var dinner = "http://cdn.innovativelanguage.com/wordlists/audio/288922.mp3"
 var key = "http://cdn.innovativelanguage.com/wordlists/audio/733210.mp3";
 var bday = "https://d1pra95f92lrn3.cloudfront.net/audio/210582.mp3";
 var xiedian = "https://d1pra95f92lrn3.cloudfront.net/audio/267470.mp3";
+var youxiu = "https://d1pra95f92lrn3.cloudfront.net/audio/211035.mp3";
 
 var wotd = [
 [cash, "出去的時候"+z+"我不喜歡帶現金。", "出去的时候"+z+"我不喜欢带现金。", "Chūqù de shíhou wǒ bù xǐhuan dài xiànjīn.", "I do not like to carry cash with me when I go out."],
@@ -17,6 +18,7 @@ var wotd = [
 [dinner, "我的家人每天"+z+"晚上一起吃晚飯。", "我的家人每天"+z+"晚上一起吃晚飯。", "Wǒ de jiārén měi tiān wǎnshàng yìqǐ chī wǎnfàn.", "My family eats dinner together every night."],
 [key, "男人正在用"+z+"鑰匙開車門。", "男人正在用"+z+"钥匙开车门。", "Nánrén zhèngzài yòng yàoshi kāi chēmén.", "The man is unlocking the car door with the key."],
 [bday, "我希望你過一個"+z+"快樂的生日。", "我希望你过一个"+z+"快乐的生日。", "Wǒ xīwàng nǐ guò yí gè kuàilè de shēngrì.", " I hope you have a happy birthday"],
-[xiedian, "這個女人正在鞋店"+z+"裏買鞋。", "这个女人正在鞋店"+z+"里买鞋。", "Zhège nǚrén zhèngzài xiédiàn lǐ mǎi xié.", "The woman is shopping for shoes at the shoe store."]
+[xiedian, "這個女人正在鞋店"+z+"裏買鞋。", "这个女人正在鞋店"+z+"里买鞋。", "Zhège nǚrén zhèngzài xiédiàn lǐ mǎi xié.", "The woman is shopping for shoes at the shoe store."],
+[youxiu, "我從來都不是優秀"+z+"的學生，但我在"+z+"學校學了很多。","我从来都不是优秀"+z+"的学生，但我在"+z+"学校学了很多。", "Wǒ cónglái dōu búshì yōuxiù de xuéshēng, dàn wǒ zài xuéxiào xuéle hěnduō.", "I was never a great student, but I still learned a lot in school."]
 
 ];
