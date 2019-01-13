78450ccc39a17dadaaa1eb693e4c594fca0670db
robf408 <robf408@yahoo.com>
2019-01-12 19:13:36 -0800

    Add missing file

---
+++ heavenly_stems.js
@@ -0,0 +1,74 @@
+﻿var z = '\u200B';
+
+var cp = "http://s3.amazonaws.com/chinesepod.com/";
+var zhijia = cp + "extra/QW0330/29169e38c9782ac6efd28ea39e04f803b962363d/mp3/64/chinesepod_6830_dialogue_62884_prototype_64461_187320.mp3";
+var heavenly_stems_pre = "<h3>10 Heavenly Stems</h3>\n" +
+"<table><tbody>\n" +
+		"<tr>\n" +
+			"<td>甲</td>\n" +
+			"<td>乙</td>\n" +
+			"<td>丙</td>\n" +
+			"<td>丁</td>\n" +
+			"<td>戊</td>\n" +
+			"<td>己</td>\n" +
+			"<td>庚</td>\n" +
+			"<td>辛</td>\n" +
+			"<td>壬</td>\n" +
+			"<td>癸</td>\n" +
+		"</tr>\n" +
+		"<tr>\n" +
+			"<td>jiǎ</td>\n" +
+			"<td>yǐ</td>\n" +
+			"<td>bǐng</td>\n" +
+			"<td>dīng</td>\n" +
+			"<td>w&ugrave;</td>\n" +
+			"<td>jǐ</td>\n" +
+			"<td>gēng</td>\n" +
+			"<td>xīn</td>\n" +
+			"<td>r&eacute;n</td>\n" +
+			"<td>guǐ</td>\n" +
+		"</tr>\n" +
+	"</tbody>\n" +
+	"</table>\n" +
+"<h3>12 Earthly Branches</h3>\n" +
+"<table>\n" +
+	"<tbody>\n" +
+		"<tr>\n" +
+			"<td>子</td>\n" +
+			"<td>丑</td>\n" +
+			"<td>寅</td>\n" +
+			"<td>卯</td>\n" +
+			"<td>辰</td>\n" +
+			"<td>巳</td>\n" +
+			"<td>午</td>\n" +
+			"<td>未</td>\n" +
+			"<td>申</td>\n" +
+			"<td>酉</td>\n" +
+			"<td>戌</td>\n" +
+			"<td>亥</td>\n" +
+		"</tr>\n" +
+		"<tr>\n" +
+			"<td>zǐ</td>\n" +
+			"<td>chǒu</td>\n" +
+			"<td>y&iacute;n</td>\n" +
+			"<td>maǒ</td>\n" +
+			"<td>ch&eacute;n</td>\n" +
+			"<td>s&igrave;</td>\n" +
+			"<td>wǔ</td>\n" +
+			"<td>we&igrave;</td>\n" +
+			"<td>shēn</td>\n" +
+			"<td>yǒu</td>\n" +
+			"<td>xū</td>\n" +
+			"<td>hài;</td>\n" +
+		"</tr>\n" +
+	"</tbody>\n" +
+"</table>\n" +
+"<h3></h3>\n"
+
+var xinku = cp + "2632/aceed97f75e6ed02cd1665ef65aa4081c9c3573d/mp3/64/chinesepod_7225_dialogue_67245_prototype_64794_36724.mp3";
+
+var heavenly_stems = [
+  [zhijia, "剪指甲", "剪指甲", "jiǎn zhǐjia", "to cut fingernails"],
+  [xinku, "吳老闆，應該要"+z+"謝謝你！辛苦了！", "吴老板，应该要"+z+"谢谢你！辛苦了！", "Wú lǎobǎn，yīnggāi yào xièxie nǐ！xīnkǔ le！", "Mr Wu, I should thank you for your hard work!"],
+  [, "壬辰", "壬辰", "rénchén", "twenty ninth year I5 of the 60 year cycle, e.g. 2012 or 2072"]
+];
