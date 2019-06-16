51cf35d718c51d53906d5b2911318a8345b3f014
robf408 <robf408@yahoo.com>
2019-06-16 11:29:25 -0700

    staffCabin01.html

--- 2019.1.js
+++ 2019.1.js
@@ -8,6 +8,8 @@ var fada = cp + "0811/19e0e448f22e02f08590c242e130eec200d93f04/mp3/64/rec-120452
 var fangshi = cp + "2329/512842065df677d2ae6310be00c59a15718297e0/mp3/64/chinesepod_6819_dialogue_62509_prototype_8044_11044.mp3";
 var jingyu = cp + "4120/18034c1995b798cc43cb53b2b1e0c8fa970401b7/mp3/64/chinesepod_8017_dialogue_76601_prototype_1540971170.mp3";
 var cunqian = cp + "3026/29740ef34af7c9941341abbe3e5818ff5591012f/mp3/64/chinesepod_7693_dialogue_72959_prototype_12146_29103_20276_34869.mp3";
+var tanhua = cp + "0965/c3bf3b50e67b7d8e0f0f4b229e46c4cd4f146925/mp3/64/rec-1218499497125-21.mp3"
+var lianxi = cp + "1149/a394b798816d3b9c0ae727db543709af72d39fe9/mp3/64/rec-1240613731531-22.mp3";
 var v20191 = [
   [quanmian, "我不是說全面八折"+z+"喔，我說的是"+z+"“部分”商品全面"+z+"八折。", "我不是说全面八折"+z+"喔，我说的是"+z+"“部分”商品全面"+z+"八折。", "wǒ bùshì shuō quánmiàn bāzhé ō，wǒ shuō de shì“bùfen”shāngpǐn quánmiàn bāzhé。", "I didn't say 20% off everything, I said it was a full 20% off \"certain\" goods."],
   [taidu, "嗯，我就欣賞"+z+"你這種態度。"+z+"敬業、專業、"+z+"自信但不驕傲。", "嗯，我就欣赏"+z+"你这种态度。"+z+"敬业、专业、"+z+"自信但不骄傲。", "én，wǒ jiù xīnshǎng nǐ zhèzhǒng tàidu。jìngyè、zhuānyè、zìxìn dàn bù jiāoào。", "Hmm, I really appreciate your attitude: hard-working, professional, confident – yet not arrogant."],
@@ -15,5 +17,7 @@ var v20191 = [
   [fada, "現在醫學這麼"+z+"發達，他的病肯定"+z+"能治好的。", "现在医学这么"+z+"发达，他的病肯定"+z+"能治好的。", "xiànzài yīxué zhème fādá，tā de bìng kěndìng néng zhì hǎo de。", "Medical science is so advanced now, his illness can definitely be cured."],
   [fangshi, "那是你們男人的"+z+"方式。", "那是你们男人的"+z+"方式。", "nà shì nǐmen nánrén de fāngshì。", "That's the way you guys do it."],
   [jingyu, "首先，對於養老院"+z+"的老人而言，他們"+z+"經常面臨無人陪伴"+z+"的尷尬境遇。年輕"+z+"志願者的出現，"+z+"解決了老年人沒人"+z+"陪的問題。", "首先，对于养老院"+z+"的老人而言，他们"+z+"经常面临无人陪伴"+z+"的尴尬境遇。年轻"+z+"志愿者的出现，"+z+"解决了老年人没人"+z+"陪的问题。", "shǒuxiān，duìyú yǎnglǎoyuàn de lǎorén éryán，tāmen jīngcháng miànlín wúrén péibàn de gāngà jìngyù。niánqīng zhìyuànzhě de chūxiàn，jiějué le lǎoniánrén méi rén péi de wèntí。", "First of all, the elderly in nursing homes often face awkward situations of being alone but the presence of young volunteers solved this problem."],
-  [cunqian, "我的朋友不多，"+z+"我也懶得去找"+z+"朋友，我喜歡"+z+"自己一個人，"+z+"比較安靜，對了，"+z+"我倒是很會存錢。", "我的朋友不多，"+z+"我也懒得去找"+z+"朋友，我喜欢"+z+"自己一个人，"+z+"比较安静，对了，"+z+"我倒是很会存钱。", "wǒ de péngyou bù duō，wǒ yě lǎnde qù zhǎo péngyou，wǒ xǐhuan zìjǐ yī ge rén，bǐjiào ānjìng，duìle，wǒ dàoshì hěn huì cúnqián。", "I don't have many friends and I get lazy about meeting up with them. I like time to myself and I'm quite quiet. Oh yeah and I'm actually the kind of guy who is quite good at saving money."]
+  [cunqian, "我的朋友不多，"+z+"我也懶得去找"+z+"朋友，我喜歡"+z+"自己一個人，"+z+"比較安靜，對了，"+z+"我倒是很會存錢。", "我的朋友不多，"+z+"我也懒得去找"+z+"朋友，我喜欢"+z+"自己一个人，"+z+"比较安静，对了，"+z+"我倒是很会存钱。", "wǒ de péngyou bù duō，wǒ yě lǎnde qù zhǎo péngyou，wǒ xǐhuan zìjǐ yī ge rén，bǐjiào ānjìng，duìle，wǒ dàoshì hěn huì cúnqián。", "I don't have many friends and I get lazy about meeting up with them. I like time to myself and I'm quite quiet. Oh yeah and I'm actually the kind of guy who is quite good at saving money."],
+  [tanhua, "跟他談話時，"+z+"我發現他的"+z+"思維模式很獨特。", "跟他谈话时，"+z+"我发现他的"+z+"思维模式很独特。", "gēn tā tánhuà shí，wǒ fāxiàn tā de sīwéi móshì hěn dútè。", "Speaking with him, I found that his thinking patterns were very unique."],
+  [lianxi, "你怎麼都不聯繫"+z+"我們？是不是把"+z+"我們都給忘記了？", "你怎么都不联系"+z+"我们？是不是把"+z+"我们都给忘记了？", "nǐ zěnme dōu bù liánxì wǒmen？shì bu shì bǎ wǒmen dōu gěi wàngjì le？", "How come you haven't gotten in touch with us? Have you forgotten about us?"]
 ];
