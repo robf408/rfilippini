faf4415ef7c91a5b9c58a360c1fd8bc122da9467
robf408 <robf408@yahoo.com>
2020-11-28 19:57:24 -0800

    Add slider to control width of incident beam.

---
+++ 2020.1.js
@@ -0,0 +1,9 @@
+﻿var z = '\u200B';
+
+var cp = "http://s3.amazonaws.com/chinesepod.com/";
+
+var zhaoji = cp + "2745/a751b1246ad4411e3b78b02a4d7efe6e36c497b1/mp3/64/chinesepod_7354_dialogue_68854_prototype_11881_12734_1796_24189.mp3";
+
+var v20201 = [
+  [zhaoji, "別著急，你小心"+z+"摸著牆走，到客廳"+z+"坐著，我去廚房的"+z+"櫃子裡拿蠟燭。", "别着急，你小心"+z+"摸着墙走，到客厅"+z+"坐着，我去厨房的"+z+"柜子里拿蜡烛。", "bié zháojí，nǐ xiǎoxīn mō zhe qiáng zǒu，dào kètīng zuò zhe，wǒ qù chúfáng de guìzi lǐ ná làzhú。", "Don't panic. Just carefully feel your way along the wall, then sit down in the living room, I'll go to the kitchen cabinet to get some candles."]
+];
